b9c1bc414397c25a2172a0c09fc57e0861f308fa
samxddICANT <samuel.goh.2021@scis.smu.edu.sg>
2023-11-09 16:12:48 +0800

    Diagramming as code
    Diagram code + archi diagram png

---
+++ dg.py
@@ -0,0 +1,89 @@
+from diagrams import Diagram, Cluster, Edge
+
+from diagrams.generic.compute import Rack
+from diagrams.onprem.client import User
+from diagrams.aws.network import Route53, VPCPeering
+from diagrams.aws.mobile import Amplify, APIGateway
+from diagrams.aws.security import Cognito, WAF, IdentityAndAccessManagementIam
+from diagrams.aws.compute import Lambda
+from diagrams.aws.database import Aurora, ElasticacheForRedis
+from diagrams.aws.storage import SimpleStorageServiceS3
+from diagrams.aws.management import Cloudwatch
+from diagrams.aws.integration import SimpleQueueServiceSqs
+from diagrams.aws.engagement import SimpleEmailServiceSes
+from diagrams.custom import Custom
+
+
+with Diagram("Architecture Diagram", show=False, direction="TB"):
+    user = User("Users")
+    ext_sys = Rack("External Systems")
+    mongodb = Custom("MongoDB","./custom_icons/mongodbicon.png")
+
+    with Cluster("AWS Cloud"):
+        rt53 = Route53("Route 53")
+        amplify = Amplify("AWS Amplify")
+        cognito = Cognito("Amazon Cognito")
+
+        [ext_sys,user] >> Edge(label = "Uses") >> rt53
+        rt53 >> amplify >> Edge(label = "User Login") >> cognito
+        
+        with Cluster("AWS API Gateway"):
+            with Cluster("Primary Region"):
+                g1 = APIGateway("AWS API Gateway")
+       
+            with Cluster("Failover Region"):
+                g2 = APIGateway("AWS API Gateway")
+
+        rt53 >> Edge(label = "HTTPS Request") >> [g1,g2]
+        amplify >> Edge(label = "HTTPS Request") >> [g1,g2]
+        [g1,g2] >> Edge(label = "Verify") >> cognito
+
+        with Cluster("Rest API"):
+            logs = Lambda("AWS Lambda")
+ 
+        with Cluster("Other services"):
+            WAF("AWS WAF")
+            Cognito("Amazon Cognito")
+            Cloudwatch("Amazon Cloudwatch")
+            IdentityAndAccessManagementIam("AWS IAM")
+            s3 = SimpleStorageServiceS3("AWS S3")
+
+        with Cluster("VPC"):
+            with Cluster("Private Subnet"):
+                with Cluster("User Storage"):
+                    with Cluster("Rest API"):
+                        user_storage = Lambda("AWS Lambda")
+            
+                with Cluster("Points Ledger"):
+                    with Cluster("Rest API"):
+                        points_ledger = Lambda("AWS Lambda")
+            
+                with Cluster("MakerChecker"):
+                    with Cluster("Rest API"):
+                        maker_checker = Lambda("AWS Lambda")
+
+                with Cluster("Primary Database"):
+                    pri_DB = Aurora("Amazon Aurora")
+            
+                with Cluster("Read Replica"):
+                    rr = Aurora("Amazon Aurora")
+            
+                pri_DB >> Edge(label = "Synchronize") >> rr
+                vpcpc = VPCPeering("VPC Peering Connection")
+                ecr = ElasticacheForRedis("ElastiCache (Redis)")
+
+        g1 >> Edge(label = "Forward Requests") >> [user_storage,points_ledger,maker_checker]
+        g2 >> Edge(label = "Forward Requests") >> [user_storage,points_ledger,maker_checker]
+        [g1,g2] >> Edge(label = "Query Logs") >> logs >> Edge(label = "Fetch Logs") >> s3
+
+        maker_checker >> ecr >> user_storage
+        maker_checker >> Edge(label = "Maker Requests Approved") >> points_ledger
+        maker_checker >> vpcpc >> mongodb
+
+        points_ledger >> Edge(label = "Read DB") >> [pri_DB, rr]
+        user_storage >> Edge(label = "Write DB") >> [pri_DB, rr]
+
+        sqs = SimpleQueueServiceSqs("AWS SQS")
+        maker_checker >> Edge(label = "Create Message") >> sqs
+        sqs >> Edge(label = "Timeout Messages") >> SimpleQueueServiceSqs("DLQ")
+        user << Edge(label = "Sends Email") << SimpleEmailServiceSes("AWS SES") << Edge(label = "Create Email") << Lambda("AWS Lambda") <<  Edge(label = "Poll Message") << sqs
